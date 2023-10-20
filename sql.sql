96cc6e582a9d7eebf25b188fcfa054cb51399f39
Shubham Chauhan <64500580+shubhamppl@users.noreply.github.com>
2023-10-20 11:29:13 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,14 +3,21 @@
  *       +-------+---------------+--------------+--------------------+---------------+       *
 
 
-SELECT  maintable_M3LJZ.ID,  maintable_M3LJZ.Name,  cb_companydivisions.DivisionName AS DivisionName,
-        t.Name AS ManagerName,maintable_M3LJZ.Salary FROM maintable_M3LJZ
-JOIN cb_companydivisions ON cb_companydivisions.id = maintable_M3LJZ.DivisionID
-JOIN ( SELECT Name,ID FROM maintable_M3LJZ
-)AS t ON t.ID = maintable_M3LJZ.ManagerID
-WHERE Salary = (SELECT MIN(Salary) FROM (SELECT  
-  * FROM maintable_M3LJZ ORDER BY salary DESC 
-LIMIT 3) AS x )
+SELECT 
+    m.ID, 
+    m.Name, 
+    d.DivisionName AS DivisionName, 
+    IFNULL(mr.Name, 'No Manager') as ManagerName, 
+    m.Salary 
+FROM maintable_M3LJZ m
+JOIN cb_companydivisions d ON m.DivisionID = d.id
+LEFT JOIN maintable_M3LJZ mr ON m.ManagerID = mr.ID
+WHERE m.Salary = (
+    SELECT DISTINCT Salary
+    FROM maintable_M3LJZ
+    ORDER BY Salary DESC
+    LIMIT 2, 1
+);
 
  *                        +-----------------+------------+------------+                      *
  *                        | Bob Boss        | 2          | 24         |                      *

a23964ecb99282685ef4f6e462c0ea9c77fc87bc
Shubham Chauhan <64500580+shubhamppl@users.noreply.github.com>
2023-10-22 11:40:05 +0530

    Update python.py

--- python.py
+++ python.py
@@ -27,7 +27,22 @@ print(FirstFactorial(int(input())))
 # Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
 # If there are two or more words that are the same length, return the first word from the string with that length.
 # Ignore punctuation and assume sen will not be empty.
+def LongestWord(sen):
+    punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    for char in punctuation:
+        sen = sen.replace(char, '')  # Remove each character in punctuation from the string
 
+    words = sen.split()  # Split the string into words
+
+    longest_word = max(words, key=len)  # Find the longest word using the max function
+
+    return longest_word
+
+# Example usage:
+sen = "Hello, this is a sample string! It includes various words."
+print(LongestWord(sen))
+
+#or 
 import re
 
 def LongestWord(sen):

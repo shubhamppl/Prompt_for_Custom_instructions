31e1f4d14f022f66b623e2718a9061f73afa5897
Shubham Chauhan <64500580+shubhamppl@users.noreply.github.com>
2023-10-25 11:29:24 +0530

    Update python_2.py

--- python_2.py
+++ python_2.py
@@ -1,3 +1,5 @@
+#Have the function Time) take the array of integers stored in , and determine if any two numbers (excluding the first element) in the array can sum up to the first element in the array. For example: if x is 17, 3, 5, 2, 4, 8, 11), then there are actually two pairs that sum to the number 7: 15, 2] and 1-4, 11). Your program should return all pairs, with the numbers separated by a comma, in the order the first number appears in the array. Pairs should be separated by a space. So for the example above, your program would return: 5,2 -4,11
+#If there are no two numbers that sum to the first element in the array, return -1
 # Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is
 # a prime n as mumber, otherwise return the string false. The range will be between 1 and 2^16.
 import math as m
